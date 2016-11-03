2dbbefd6084637f29ae4196307469784dc6d0e3a
alex.sharov <alex.sharov@lazada.com>
2016-11-03 17:56:52 +0700

    init

--- service/service.go
+++ service/service.go
@@ -28,6 +28,10 @@ func ToContext(ctx context.Context, factory *Factory) context.Context {
 	return context.WithValue(ctx, KEY_FACTORY, factory)
 }
 
+func FromContext(ctx context.Context) *Factory{
+	return ctx.Value(KEY_FACTORY).(*Factory)
+}
+
 func copy(ctx context.Context, from interface{}) interface{} {
 	val := reflect.ValueOf(from)
 	if val.Kind() == reflect.Ptr {
@@ -39,9 +43,9 @@ func copy(ctx context.Context, from interface{}) interface{} {
 }
 
 func A(ctx context.Context) IA {
-	return copy(ctx, ctx.Value(KEY_FACTORY).(*Factory).A).(IA)
+	return copy(ctx, FromContext(ctx).A).(IA)
 }
 
 func B(ctx context.Context) IB {
-	return copy(ctx, ctx.Value(KEY_FACTORY).(*Factory).B).(IB)
+	return copy(ctx, FromContext(ctx).B).(IB)
 }
